268c15b2557f4440fe56f5bda2bf83eb406d1a1f
Shahmeer Khan <shahmirkhan519@gmail.com>
2024-01-04 18:10:01 +0500

    created payload accept API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app = FastAPI()
 
@@ -13,5 +15,6 @@ async def get_posts():
     return {"data": "This is your post"} #FastAPI directly converts this into JSON 
 
 @app.post("/createposts")
-async def create_posts():
+async def create_posts(payload: dict = Body(...)):
+    print(payload)
     return {"message": "successfully created post"} #FastAPI directly converts this into JSON 
