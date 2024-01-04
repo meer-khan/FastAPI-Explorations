657596ca404175ad8094ac349af5d35ec5759097
Shahmeer Khan <shahmirkhan519@gmail.com>
2024-01-04 18:03:09 +0500

    working on apis

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"} #FastAPI directly converts this into JSON 
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "This is your post"} #FastAPI directly converts this into JSON 
+
+@app.post("/createposts")
+async def create_posts():
+    return {"message": "successfully created post"} #FastAPI directly converts this into JSON 
