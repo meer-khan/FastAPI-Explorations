d7e97ac3b10f576161cfb921a681686b73268786
meer-khan <shahmirkhan519@gmail.com>
2024-01-07 20:56:43 +0500

    working on data validations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.params import Body
-
+from models.post_model import Post
 
 app = FastAPI()
 
@@ -15,6 +15,11 @@ async def get_posts():
     return {"data": "This is your post"} #FastAPI directly converts this into JSON 
 
 @app.post("/createposts")
-async def create_posts(payload: dict = Body(...)):
-    print(payload)
+# async def create_posts(payload: dict = Body(...)):
+async def create_posts(new_post: Post):
+
+    print(new_post)
+    print(new_post.title)
+
+
     return {"message": "successfully created post"} #FastAPI directly converts this into JSON 
