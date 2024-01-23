a00e1eda19e92759fb096be18a162fc4838478f8
meer-khan <shahmirkhan519@gmail.com>
2024-01-24 01:49:26 +0500

    working on pydantic modelling

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from post_model import Post
+from app.schemas import Post
 import uuid
 from icecream import ic
 import psycopg2
@@ -13,15 +13,20 @@ from database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 
-
 app = FastAPI()
 
 try:
-    conn = psycopg2.connect(host= 'localhost' , dbname= "fastapi", 
-                        user='postgres', port=5432, password=config("DB_PASSWORD"), 
-                        cursor_factory=RealDictCursor)
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname="fastapi",
+        user="postgres",
+        port=5432,
+        password=config("DB_PASSWORD"),
+        cursor_factory=RealDictCursor,
+    )
     cursor = conn.cursor()
     print("Database connection was successfull")
+
 except Exception as ex:
     raise Exception(ex)
 
@@ -31,84 +36,91 @@ my_posts = [
         "content": "A advance generation shoe for running",
         "published": True,
         "rating": 5,
-        "id": "bf50381a-9e8c-4b3c-9f66-259192ff424a"
+        "id": "bf50381a-9e8c-4b3c-9f66-259192ff424a",
     },
     {
         "title": "Nike Pegasus 39",
         "content": "A cool looking shoe for running",
         "published": True,
         "rating": 5,
-        "id": "f2615b82-f2ff-4003-96f0-ef05eca94ce6"
+        "id": "f2615b82-f2ff-4003-96f0-ef05eca94ce6",
     },
-        {
+    {
         "title": "Nike Pegasus 39",
         "content": "A cool looking shoe for running",
         "published": True,
         "rating": 5,
-        "id": 123
-    }
-    ]
+        "id": 123,
+    },
+]
 
-def find_post(id): 
-    for i in my_posts: 
+
+def find_post(id):
+    for i in my_posts:
         if i["id"] == id:
             return i
 
+
 def find_index_post(id):
-    for i , p in enumerate(my_posts):
+    for i, p in enumerate(my_posts):
         if p["id"] == id:
             return i
-        
-@app.get("/sqlalchemy")
-async def test(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    print(type(posts))
-    print(posts)
-    return {"status": posts}
-
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"} #FastAPI directly converts this into JSON 
+    return {"message": "Hello World"}  # FastAPI directly converts this into JSON
 
 
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("SELECT * FROM posts")
-    posts = cursor.fetchall()
-    return {"data": posts} #FastAPI directly converts this into JSON 
+async def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("SELECT * FROM posts")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return {"data": posts}  # FastAPI directly converts this into JSON
 
 
 # create post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 # async def create_posts(payload: dict = Body(...)):
-async def create_posts(post: Post):
+async def create_posts(post: Post, db: Session = Depends(get_db)):
     # * %s protects us from SQL injections
-        # We can write query like INSERT INTO posts (title, content, published) VALUSE ({title}, {content}, {published})
-        # using the f string but some user can pass the SQL statements directly into these variables 
-        # to prevent this we use %s statement to sanitize the values coming from the user's end and then pass to the query
-    
+    # We can write query like INSERT INTO posts (title, content, published) VALUSE ({title}, {content}, {published})
+    # using the f string but some user can pass the SQL statements directly into these variables
+    # to prevent this we use %s statement to sanitize the values coming from the user's end and then pass to the query
+
     # RETURNING * will return the newly created object
-    cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", 
-                   (post.title, post.content, post.published) ) 
-    new_post = cursor.fetchone()
+    # cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
+    #                (post.title, post.content, post.published) )
+    # new_post = cursor.fetchone()
 
     # it will store information in the database
-    conn.commit()
+    # conn.commit()
 
-    return {"message": new_post} #FastAPI directly converts this into JSON 
+    # new_post = models.Post(
+    #     title=post.title, content=post.content, published=post.published
+    # )
+    # Let's say we have 50 fields in the Model, and it is not a good approach to pass 50 values to the model
+    # what we can do it, we can unpack the post(pydantic) dictionary and pass directly to the Post Model
+    new_post = models.Post(**post.model_dump(exclude="rating"))
+
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return {"message": new_post}  # FastAPI directly converts this into JSON
 
 
 @app.get("/posts/{id}")
-async def get_posts(id:int,response: Response):
-    # We cannot use VALUES keyword in the select statement, this is against SQL syntax. 
+async def get_posts(id: int, response: Response, db: Session = Depends(get_db)):
+    # We cannot use VALUES keyword in the select statement, this is against SQL syntax.
     # VALUES keyword is specifically designed for the INSERT Statements
-    cursor.execute("SELECT * FROM posts WHERE id = (%s)", (id,))
-    post = cursor.fetchone()
-    if not post: 
+    # cursor.execute("SELECT * FROM posts WHERE id = (%s)", (id,))
+    # post = cursor.fetchone()
 
-        # *FIRST WAY TO SETTING RESPONSE 
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        # *FIRST WAY TO SETTING RESPONSE
         # response.status_code = 404
         # return {"msg": f"Post with id {id} not found"}
 
@@ -116,31 +128,53 @@ async def get_posts(id:int,response: Response):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"msg": f"Post with id {id} not found"}
 
-        #* 3rd OF SETTING RESPONSE
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
-    
-        #* 4th way is described in the delete method
+        # * 3rd OF SETTING RESPONSE
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+
+        # * 4th way is described in the delete method
         # in which we set the status code in the decorator
     return {"data": post}
 
+
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
-    cursor.execute("DELETE FROM posts WHERE id = (%s) RETURNING *", (id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if not deleted_post: 
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
-    # return {"msg": "post was successfully deleted"} 
-    # * We donot need to return anything on delete request, even if we return anything, FastAPI will not return anything by default 
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("DELETE FROM posts WHERE id = (%s) RETURNING *", (id,))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+
+    post = db.query(models.Post).filter(models.Post.id == id)
+
+    if post.first() == None:
+        # if not deleted_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+    # return {"msg": "post was successfully deleted"}
+    # * We donot need to return anything on delete request, even if we return anything, FastAPI will not return anything by default
     # So there is no point of writing return statement on delete request
+    post.delete(synchronize_session=False)
+    db.commit()
 
 
 @app.put("/posts/{id}")
-def update_post(id:int, post:Post):
-    cursor.execute("UPDATE posts SET title=%s, content = %s, published = %s WHERE id = %s RETURNING *",
-                   (post.title, post.content, post.published, id))
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if updated_post is None: 
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found")
-    return {"message" : updated_post}
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     "UPDATE posts SET title=%s, content = %s, published = %s WHERE id = %s RETURNING *",
+    #     (post.title, post.content, post.published, id),
+    # )
+    # post = cursor.fetchone()
+    # conn.commit()
+
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_updated = post_query.first()
+
+    if post_updated is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
+        )
+
+    post_query.update(post.model_dump(exclude="rating"), synchronize_session=False)
+    db.commit()
+    return {"message": post_query.first()}
