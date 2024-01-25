68216f70561ac38a0626c0148b5a18fe9e05b426
meer-khan <shahmirkhan519@gmail.com>
2024-01-26 01:10:12 +0500

    created 2 new schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Strict, Field
+from pydantic import BaseModel, Strict, Field, EmailStr
 from typing import Optional
 from datetime import datetime
 class PostBase(BaseModel):
@@ -13,3 +13,32 @@ class Post(PostBase):
     # content: str
     # published: bool = True
     created_at : datetime
+
+
+class UserCreate(BaseModel): 
+    email: EmailStr
+    password: str 
+    # @model_validator(mode='after')
+    # def check_passwords_match(self) -> 'UserModel':
+    #     pw1 = self.password1
+    #     pw2 = self.password2
+    #     if pw1 is not None and pw2 is not None and pw1 != pw2:
+    #         raise ValueError('passwords do not match')
+    #     return self
+
+    # @root_validator()
+    # def verify_password_match(cls,values):
+    #     password = values.get("password")
+    #     confirm_password = values.get("confirm_password")
+
+    #     if password != confirm_password:
+    #         raise ValueError("The two passwords did not match.")
+    #     return values
+
+class UserOut(UserCreate): 
+    id:int
+    email: EmailStr
+
+    # class Config:
+    #     orm_mode = True
+    
