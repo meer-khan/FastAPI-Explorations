87c9bae175efb7fb49ddbc73c7358dbc0cbbbe60
meer-khan <shahmirkhan519@gmail.com>
2024-01-28 17:31:17 +0500

    Integrated routes in main

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ from decouple import config
 import models
 from database import engine, get_db
 from typing import List
-from passlib.context import CryptContext
+import utils
+from routers import users, posts
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -74,137 +74,6 @@ async def root():
     return {"message": "Hello World"}  # FastAPI directly converts this into JSON
 
 
-# we can use List from typing library and we can also use list[Post] from python directly
-# Reference: https://fastapi.tiangolo.com/tutorial/response-model/#response_model-parameter
-@app.get("/posts", response_model=List[Post])
-async def get_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    # return  posts  # FastAPI directly converts this into JSON
-    return posts
 
-
-# create post
-@app.post(
-    "/posts",
-    status_code=status.HTTP_201_CREATED,
-    response_model=Post,
-    response_model_exclude=["title", "content"],
-)
-# async def create_posts(payload: dict = Body(...)):
-async def create_posts(post: PostBase, db: Session = Depends(get_db)):
-    # * %s protects us from SQL injections
-    # We can write query like INSERT INTO posts (title, content, published) VALUSE ({title}, {content}, {published})
-    # using the f string but some user can pass the SQL statements directly into these variables
-    # to prevent this we use %s statement to sanitize the values coming from the user's end and then pass to the query
-
-    # RETURNING * will return the newly created object
-    # cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *",
-    #                (post.title, post.content, post.published) )
-    # new_post = cursor.fetchone()
-
-    # it will store information in the database
-    # conn.commit()
-
-    # new_post = models.Post(
-    #     title=post.title, content=post.content, published=post.published
-    # )
-    # Let's say we have 50 fields in the Model, and it is not a good approach to pass 50 values to the model
-    # what we can do it, we can unpack the post(pydantic) dictionary and pass directly to the Post Model
-    new_post = models.Post(**post.model_dump())
-
-    db.add(new_post)
-    db.commit()
-    db.refresh(new_post)
-    ic(new_post)
-    ic(type(new_post))
-    ic(new_post.title)
-    return new_post  # FastAPI directly converts this into JSON
-
-
-@app.get("/posts/{id}", response_model=Post)
-async def get_posts(id: int, response: Response, db: Session = Depends(get_db)):
-    # We cannot use VALUES keyword in the select statement, this is against SQL syntax.
-    # VALUES keyword is specifically designed for the INSERT Statements
-    # cursor.execute("SELECT * FROM posts WHERE id = (%s)", (id,))
-    # post = cursor.fetchone()
-
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        # *FIRST WAY TO SETTING RESPONSE
-        # response.status_code = 404
-        # return {"msg": f"Post with id {id} not found"}
-
-        # *2nd WAY OF SETTING RESPONSE
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"msg": f"Post with id {id} not found"}
-
-        # * 3rd OF SETTING RESPONSE
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
-        )
-
-        # * 4th way is described in the delete method
-        # in which we set the status code in the decorator
-    return post
-
-
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("DELETE FROM posts WHERE id = (%s) RETURNING *", (id,))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
-    post = db.query(models.Post).filter(models.Post.id == id)
-
-    if post.first() == None:
-        # if not deleted_post:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
-        )
-    # return {"msg": "post was successfully deleted"}
-    # * We donot need to return anything on delete request, even if we return anything, FastAPI will not return anything by default
-    # So there is no point of writing return statement on delete request
-    post.delete(synchronize_session=False)
-    db.commit()
-
-
-@app.put("/posts/{id}", status_code=status.HTTP_200_OK, response_model=Post)
-def update_post(id: int, post: PostBase, db: Session = Depends(get_db)):
-    # cursor.execute(
-    #     "UPDATE posts SET title=%s, content = %s, published = %s WHERE id = %s RETURNING *",
-    #     (post.title, post.content, post.published, id),
-    # )
-    # post = cursor.fetchone()
-    # conn.commit()
-
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post_updated = post_query.first()
-
-    if post_updated is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {id} not found"
-        )
-
-    post_query.update(post.model_dump(exclude="rating"), synchronize_session=False)
-    db.commit()
-    return post_query.first()
-
-
-@app.post(
-    "/users",
-    status_code=status.HTTP_201_CREATED,
-    response_model=UserOut,
-    response_model_exclude=["password", "id"],
-)
-async def create_posts(user: UserCreate, db: Session = Depends(get_db)):
-
-    hashed_password = pwd_context.hash(user.password)
-    user.password = hashed_password
-
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+app.include_router(posts.router)
+app.include_router(users.router)
